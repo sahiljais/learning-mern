5694277d080fe260dfb119cb974736d1ddd8f825
Sahil Jaiswal <sahiljaiswal1398@gmail.com>
2022-05-05 01:15:53 +0530

    feat - authentication and protected routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const router =  express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controller/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.get('/', getGoals)
-
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports =  router
