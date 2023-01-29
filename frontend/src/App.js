1b0d29c2bccdfbd987ad6c428b5681bbd0305d0b
Sahil Jaiswal <sahiljaiswal1398@gmail.com>
2023-01-30 03:27:36 +0530

    feat - set up redux toolkit, auth slice and register API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -18,6 +20,7 @@ function App() {
       </Routes>
       </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,10 +1,33 @@
 import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 import { FaUser } from "react-icons/fa"
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 const Register = () => {
     const [formData,setFormData] = useState({name: '',email:'',password:'',password2:''})
 
-    const {name,email,password,password2} = formData
+    const {name, email, password, password2} = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user, isError, isSuccess, isLoading, message } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError){
+            toast.error(message)
+        }
+
+        if(isSuccess || user){
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -15,6 +38,22 @@ const Register = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        if(password !== password2){
+            toast.error('Passwords do not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password
+            }
+
+            dispatch(register(userData))
+        }
+    }
+
+    if(isLoading){
+        return <Spinner />
     }
 
   return (
@@ -36,7 +75,7 @@ const Register = () => {
             <input type="password" className="form-control" id="password" name="password" value={password} placeholder='Enter your password' onChange={onChange} />
             </div>
             <div className="form-group">
-            <input type="password2" className="form-control" id="password2" name="password2" value={password2} placeholder='Confirm password' onChange={onChange} />
+            <input type="password" className="form-control" id="password2" name="password2" value={password2} placeholder='Confirm password' onChange={onChange} />
             </div>
             <div className="form-group">
                 <button type="submit" className="btn btn-block">Submit</button>
